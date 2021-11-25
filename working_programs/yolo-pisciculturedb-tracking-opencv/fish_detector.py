568bbbfa7791edbeba9e00be0bf1f998b0cab995
alejandroariaszuluaga <alejandroariaszuluaga@gmail.com>
2021-11-25 11:10:00 -0500

    Including video index

--- working_programs/yolo-pisciculturedb-tracking-opencv/fish_detector.py
+++ working_programs/yolo-pisciculturedb-tracking-opencv/fish_detector.py
@@ -13,7 +13,6 @@
 
 # import the necessary packages
 from numpy.core.fromnumeric import round_
-from posix import times_result
 from pyimagesearch.centroidtracker import CentroidTracker
 from pyimagesearch.trackableobject import TrackableObject
 from imutils.video import VideoStream
@@ -21,15 +20,12 @@ from imutils.video import FPS
 import numpy as np
 import argparse
 import imutils
-import time
 import dlib
 import cv2
 import glob
 import os
 from datetime import datetime
-import csv
 import pandas as pd
-import statistics
 # from sklearn.metrics import pairwise_distances
 from scipy.spatial import distance_matrix
 
@@ -374,7 +370,8 @@ for j in range(len(vs_list)):
 	avg_x = np.mean(list_traveled_x) # Average X position
 	avg_y = np.mean(list_traveled_y) # Average Y position
 	csv_path = os.path.join(os.getcwd(),'video_data.csv')
-	df = pd.DataFrame([{'Average Distance': round(avg_dist,4),
+	df = pd.DataFrame([{'Video Index': j,
+						'Average Distance': round(avg_dist,4),
 						'Average X': round(avg_x),
 						'Average Y': round(avg_y),
 						'Average Pairwise Distance': round(avg_pwdist,4),
